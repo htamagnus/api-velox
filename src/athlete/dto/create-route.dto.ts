cbba181cef8c367ccbe2159f2047cf1269208216
Ágatha <agathamagnusg@hotmail.com>
2025-03-26 13:42:50 -0300

    add create route dto

---
+++ src/athlete/dto/create-route.dto.ts
@@ -0,0 +1,22 @@
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
+
+const createRouteInputDto = z.object({
+  origin: z.string(),
+  destination: z.string(),
+  modality: z.enum(['road', 'mtb']),
+});
+
+const createRouteResponseDto = z.object({
+  distanceKm: z.number(),
+  estimatedTimeMinutes: z.number(),
+  estimatedCalories: z.number(),
+  polyline: z.string(),
+});
+
+export class GetPlannedRouteInputDto extends createZodDto(
+  createRouteInputDto,
+) {}
+export class GetPlannedRouteResponseDto extends createZodDto(
+  createRouteResponseDto,
+) {}
