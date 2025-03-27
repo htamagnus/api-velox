9421645958fa9e5a7544e22c709b026d0d7c5845
Ágatha <agathamagnusg@hotmail.com>
2025-03-27 10:59:52 -0300

    add new entity in import

--- src/athlete/athlete.module.ts
+++ src/athlete/athlete.module.ts
@@ -6,9 +6,10 @@ import { AthleteEntity } from './entities/athlete.entity';
 import StravaClient from 'src/clients/strava.client';
 import GoogleMapsClient from 'src/clients/google-maps.client';
 import { JwtService } from 'src/utils/generate-jwt.utils';
+import { SavedRouteEntity } from './entities/saved-routes.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AthleteEntity])],
+  imports: [TypeOrmModule.forFeature([AthleteEntity, SavedRouteEntity])],
   controllers: [AthleteController],
   providers: [AthleteService, StravaClient, GoogleMapsClient, JwtService],
 })
