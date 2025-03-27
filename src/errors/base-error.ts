0080e914d977dd62d6b624db0b733f9e2d4f7360
Ágatha <agathamagnusg@hotmail.com>
2025-03-27 08:42:53 -0300

    create a base error

---
+++ src/errors/base-error.ts
@@ -0,0 +1,32 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export interface AppErrorOptions {
+  message: string;
+  status?: HttpStatus;
+  code?: string;
+  context?: Record<string, any>;
+}
+
+export class BaseAppError extends HttpException {
+  public readonly code?: string;
+  public readonly context?: Record<string, any>;
+
+  constructor({
+    message,
+    status = HttpStatus.BAD_REQUEST,
+    code,
+    context,
+  }: AppErrorOptions) {
+    super(
+      {
+        message,
+        code,
+        status,
+        context,
+      },
+      status,
+    );
+    this.code = code;
+    this.context = context;
+  }
+}
