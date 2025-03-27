fd81215e1445117b578ccb0d5b5afce1df72edb4
Ágatha <agathamagnusg@hotmail.com>
2025-03-27 10:58:58 -0300

    add new entity to save routes

---
+++ src/athlete/entities/saved-routes.entity.ts
@@ -0,0 +1,52 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { AthleteEntity } from './athlete.entity';
+
+@Entity('saved_routes')
+export class SavedRouteEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => AthleteEntity, (athlete) => athlete.savedRoutes)
+  @JoinColumn({ name: 'athlete_id' })
+  athlete: AthleteEntity;
+
+  @Column({ name: 'athlete_id' })
+  athleteId: string;
+
+  @Column('varchar')
+  origin: string;
+
+  @Column('varchar')
+  destination: string;
+
+  @Column('varchar')
+  modality: 'road' | 'mtb';
+
+  @Column('float')
+  distanceKm: number;
+
+  @Column('int')
+  estimatedTimeMinutes: number;
+
+  @Column('int')
+  estimatedCalories: number;
+
+  @Column('int')
+  elevationGain: number;
+
+  @Column('int')
+  elevationLoss: number;
+
+  @Column('text')
+  polyline: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
