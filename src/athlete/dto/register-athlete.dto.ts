7cb5c8cbf85c94af3298eb03608840a3093966c3
Ágatha <agathamagnusg@hotmail.com>
2025-03-27 08:42:22 -0300

    create register athlete dto

---
+++ src/athlete/dto/register-athlete.dto.ts
@@ -0,0 +1,13 @@
+import { createZodDto } from 'nestjs-zod';
+import { passwordValidationMessage, strongPasswordRegex } from 'src/utils';
+import { z } from 'zod';
+
+const registerAthleteSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, 'A senha deve ter no mínimo 8 caracteres.')
+    .regex(strongPasswordRegex, passwordValidationMessage),
+});
+
+export class RegisterAthleteDto extends createZodDto(registerAthleteSchema) {}
